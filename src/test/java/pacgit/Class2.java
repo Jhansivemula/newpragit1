1676baa412bd3ea0d1fa66f65433019f75dbc008
jhansivemula <vjhansi450@gmail.com>
2024-01-08 11:17:24 +0530

    i added one class

---
+++ src/test/java/pacgit/Class2.java
@@ -0,0 +1,13 @@
+package pacgit;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi iam jhansi");
+		System.out.println("i want to looking for softwate testing job ");
+		System.out.println("i passion about it field");
+		
+
+	}
+
+}
